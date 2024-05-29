f52400926b8d43897aaf8bb3263df44b94b6ef25
b4ry <barry90@interia.pl>
2024-05-29 21:07:55 +0200

    Add project files.

---
+++ UdpClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient();
+
+            try
+            {
+                string message = "";
+
+                (IPAddress serverIp, int serverPort) = (IPAddress.Parse(args[1].Split('=')[1]), int.Parse(args[0].Split('=')[1]));
+                IPEndPoint serverIPEndpoint = new IPEndPoint(serverIp, serverPort);
+
+                client.Connect(serverIPEndpoint);
+
+                IPEndPoint listenerEndpoint = new IPEndPoint(IPAddress.Any, 0);
+                byte[] sendBytes;
+
+                while (message != "q")
+                {
+                    Console.Write(":");
+                    message = Console.ReadLine();
+                    sendBytes = Encoding.ASCII.GetBytes(message);
+
+                    client.Send(sendBytes, sendBytes.Length);
+                    Console.WriteLine($"Sent: {message}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+    }
+}
