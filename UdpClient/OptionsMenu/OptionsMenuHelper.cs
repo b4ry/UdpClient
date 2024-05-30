436b46ea0466ca327892cfc43ba1d15154ed7a97
b4ry <barry90@interia.pl>
2024-05-30 14:33:17 +0200

    - add options menu helper, - add options, - add logic handling different options, - add task querying for delayed udp messages

---
+++ UdpClient/OptionsMenu/OptionsMenuHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Client.OptionsMenu
+{
+    internal static class OptionsMenuHelper
+    {
+        internal static void DisplayOptionsMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("------------------ OPTIONS ------------------");
+            Console.WriteLine("-o                     - display options menu,");
+            Console.WriteLine("-lu                    - get a list of actively registered users,");
+            Console.WriteLine("-dm message -NICK_NAME - send a direct message to a particular user, eg. Hello! -John");
+            Console.WriteLine("-q                     - close the application.");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("---------------------------------------------");
+        }
+    }
+}
--- UdpClient/Program.cs
+++ UdpClient/Program.cs
@@ -1,90 +1,153 @@
-﻿using System;
+﻿using Client.OptionsMenu;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Client
 {
     internal class Program
     {
+        private const string _pattern = @"^-\S+";
+
         static void Main(string[] args)
         {
-            using (UdpClient client = new UdpClient())
-            {
-                bool isRegistered = false;
+            UdpClient client = new UdpClient();
 
-                try
+            (IPAddress serverIp, int serverPort) = (IPAddress.Parse(args[1].Split('=')[1]), int.Parse(args[0].Split('=')[1]));
+            IPEndPoint serverIPEndpoint = new IPEndPoint(serverIp, serverPort);
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            Task receivingTask = new Task(() =>
+            { // UDP does not ensure that the data will be received straight away (or ever), so we need to query it
+                while (true)
                 {
-                    string message = string.Empty;
-
-                    (IPAddress serverIp, int serverPort) = (IPAddress.Parse(args[1].Split('=')[1]), int.Parse(args[0].Split('=')[1]));
-                    IPEndPoint serverIPEndpoint = new IPEndPoint(serverIp, serverPort);
-
-                    client.Connect(serverIPEndpoint); // since it is UDP, the connect DOES NOT establish a session
-
-                    byte[] sendBytes;
-
-                    string nickName = string.Empty;
-
-                    while (!isRegistered)
+                    if(cts.Token.IsCancellationRequested)
                     {
-                        while (string.IsNullOrWhiteSpace(nickName))
-                        {
-                            Console.ForegroundColor = ConsoleColor.White;
-                            Console.Write("Provide your nick:");
-                            nickName = Console.ReadLine();
-                        }
-
-                        sendBytes = Encoding.ASCII.GetBytes("UR:" + nickName);
-                        client.Send(sendBytes, sendBytes.Length);
-
-                        while (client.Available == 0)
-                        {
-                            continue;
-                        }
-
-                        var receivedData = client.Receive(ref serverIPEndpoint);
-                        var decodedReceivedData = Encoding.ASCII.GetString(receivedData);
-
-                        if (decodedReceivedData.StartsWith("RegistrationFailed:"))
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine(decodedReceivedData);
-                            nickName = string.Empty;
-                        }
-                        else
-                        {
-                            isRegistered = true;
-                        }
+                        break;
                     }
 
-                    Console.WriteLine($"Registered as: {nickName}, IP {client.Client.LocalEndPoint}");
-
-                    while (message != "q")
+                    if (client.Available > 0)
                     {
-                        PrepareMessage(out message, out sendBytes);
-
-                        client.Send(sendBytes, sendBytes.Length);
-                        Console.ForegroundColor = ConsoleColor.DarkGreen;
-                        Console.WriteLine($"Sent: {message}");
-
                         var receivedData = client.Receive(ref serverIPEndpoint);
                         var decodedReceivedData = Encoding.ASCII.GetString(receivedData);
                         Console.ForegroundColor = ConsoleColor.DarkYellow;
-                        Console.WriteLine($"Received: {decodedReceivedData}");
+                        Console.WriteLine($"{decodedReceivedData}");
+                    }
+
+                    Task.Delay(2000);
+                }
+            }, cts.Token, TaskCreationOptions.LongRunning);
+
+            bool isRegistered = false;
+
+            try
+            {
+                client.Connect(serverIPEndpoint); // since it is UDP, the connect DOES NOT establish a session
+
+                byte[] sendBytes;
+
+                string nickName = string.Empty;
+                string message = string.Empty;
+
+                while (!isRegistered)
+                {
+                    while (string.IsNullOrWhiteSpace(nickName))
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("Provide your nick:");
+                        nickName = Console.ReadLine();
+                    }
+
+                    sendBytes = Encoding.ASCII.GetBytes("-ru " + nickName);
+                    client.Send(sendBytes, sendBytes.Length);
+
+                    while (client.Available == 0)
+                    {
+                        continue;
+                    }
+
+                    var receivedData = client.Receive(ref serverIPEndpoint);
+                    var decodedReceivedData = Encoding.ASCII.GetString(receivedData);
+
+                    if (decodedReceivedData.StartsWith("RegistrationFailed:"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(decodedReceivedData);
+                        nickName = string.Empty;
+                    }
+                    else
+                    {
+                        isRegistered = true;
                     }
                 }
-                catch (Exception e)
+
+                Console.WriteLine($"Registered as: {nickName}, IP {client.Client.LocalEndPoint}");
+
+                OptionsMenuHelper.DisplayOptionsMenu();
+
+                receivingTask.Start();
+
+                while (message != "-q")
                 {
-                    Console.WriteLine(e.ToString());
+                    PrepareMessage(out message, out sendBytes);
+                    Match match = Regex.Match(message, _pattern);
+
+                    if (match.Success)
+                    {
+                        switch (match.Value)
+                        {
+                            case "-lu":
+                            case "-dm":
+                                client.Send(sendBytes, sendBytes.Length);
+                                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                                Console.WriteLine($"Sent: {message}");
+
+                                break;
+                            case "-o":
+                                OptionsMenuHelper.DisplayOptionsMenu();
+
+                                break;
+                            case "-q":
+                                break;
+                            default:
+                                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                                Console.WriteLine($"Unrecognized option!");
+
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        Console.WriteLine($"Unrecognized option!");
+                    }
                 }
             }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                cts.Cancel();
+                cts.Dispose();
+                client.Close();
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Client stopped.");
+            Console.ReadKey();
         }
 
         private static void PrepareMessage(out string message, out byte[] sendBytes)
         {
             Console.ForegroundColor = ConsoleColor.White;
-            Console.Write(":");
             message = Console.ReadLine();
             sendBytes = Encoding.ASCII.GetBytes(message);
         }
