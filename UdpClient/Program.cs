2119e28e7fe2c76f0a35c41cad57ef9f83d260c7
b4ry <barry90@interia.pl>
2024-05-30 10:28:30 +0200

    - add user registration, - refactor

--- UdpClient/Program.cs
+++ UdpClient/Program.cs
@@ -9,38 +9,75 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            UdpClient client = new UdpClient();
-
-            try
+            using (UdpClient client = new UdpClient())
             {
-                string message = "";
+                bool isRegistered = false;
 
-                (IPAddress serverIp, int serverPort) = (IPAddress.Parse(args[1].Split('=')[1]), int.Parse(args[0].Split('=')[1]));
-                IPEndPoint serverIPEndpoint = new IPEndPoint(serverIp, serverPort);
-
-                client.Connect(serverIPEndpoint);
-
-                IPEndPoint listenerEndpoint = new IPEndPoint(IPAddress.Any, 0);
-                byte[] sendBytes;
-
-                while (message != "q")
+                try
                 {
-                    Console.Write(":");
-                    message = Console.ReadLine();
-                    sendBytes = Encoding.ASCII.GetBytes(message);
+                    string message = string.Empty;
 
-                    client.Send(sendBytes, sendBytes.Length);
-                    Console.WriteLine($"Sent: {message}");
+                    (IPAddress serverIp, int serverPort) = (IPAddress.Parse(args[1].Split('=')[1]), int.Parse(args[0].Split('=')[1]));
+                    IPEndPoint serverIPEndpoint = new IPEndPoint(serverIp, serverPort);
+
+                    client.Connect(serverIPEndpoint); // since it is UDP, the connect DOES NOT establish a session
+
+                    byte[] sendBytes;
+
+                    string nickName = string.Empty;
+
+                    while (!isRegistered)
+                    {
+                        while (string.IsNullOrWhiteSpace(nickName))
+                        {
+                            Console.Write("Provide your nick:");
+                            nickName = Console.ReadLine();
+                        }
+
+                        sendBytes = Encoding.ASCII.GetBytes("UserRegistration:" + nickName);
+                        client.Send(sendBytes, sendBytes.Length);
+
+                        while (client.Available == 0)
+                        {
+                            continue;
+                        }
+
+                        var receivedData = client.Receive(ref serverIPEndpoint);
+                        var decodedReceivedData = Encoding.ASCII.GetString(receivedData);
+
+                        if (decodedReceivedData.StartsWith("RegistrationFailed:"))
+                        {
+                            Console.WriteLine(decodedReceivedData);
+                            nickName = string.Empty;
+                        }
+                        else
+                        {
+                            isRegistered = true;
+                        }
+                    }
+
+                    Console.WriteLine($"Registered as: {nickName}, IP {client.Client.LocalEndPoint}");
+
+                    while (message != "q")
+                    {
+                        PrepareMessage(out message, out sendBytes);
+
+                        client.Send(sendBytes, sendBytes.Length);
+                        Console.WriteLine($"Sent: {message}");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
                 }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-            finally
-            {
-                client.Close();
-            }
+        }
+
+        private static void PrepareMessage(out string message, out byte[] sendBytes)
+        {
+            Console.Write(":");
+            message = Console.ReadLine();
+            sendBytes = Encoding.ASCII.GetBytes(message);
         }
     }
 }
